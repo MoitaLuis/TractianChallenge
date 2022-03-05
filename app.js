91a6889a7b69a3220717ffeebe8b913ddb67e9b7
MoitaLuis <luismoita1337@gmail.com>
2022-03-05 03:45:10 -0300

    post funcionando

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const Post = require('./schemas/post');
+const bodyParser = require('body-parser');
 
 require('dotenv/config');
+
+app.use(bodyParser.json());
 
 //create read update delete
 app.get('/', (req, res) => {
     res.send('oi');
 });
+
+app.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    try{
+        const result = await post.save();
+        post.save();
+        res.json(result);
+
+    }catch(err){
+        res.json({message: err});
+    }
+
+});
+
 
 mongoose.connect(process.env.DB_CONNECTION, () => console.log("Connected to database"));
 
